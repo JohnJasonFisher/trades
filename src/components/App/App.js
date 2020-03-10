c655c31bcd41a8b4e3c33a841e509d0e9427c66f
JohnJasonFisher <jjfisher91@gmail.com>
2020-03-10 17:29:32 -0500

    Move fakeData into App Comp

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,34 @@
-import React from 'react'
+import React, {useState} from 'react'
 import TradesTable from '../TradesTable/TradesTable'
 
 function App() {
+
+	const [fields] = useState(["symbol",
+	"price",
+	"shares",
+	"action"])
+
+	const [fakeData] = useState([
+		{
+			"symbol": "SPX",
+			"price": 2780,
+			"shares": 2,
+			"action": "BUY"
+		},
+		{
+			"symbol": "SPX",
+			"price": 2770,
+			"shares": 2,
+			"action": "SELL"
+		}
+	])
+
 	return (
 	<div className="App">
 		<header className="App-header" style={{'text-align': 'center'}}>
 			<h1>Trades Project</h1>
 		</header>
-		<TradesTable />
+		<TradesTable fields={fields} trades={fakeData}/>
 	</div>
 	)
 }
