cc52544079e77ae41f023d35cd8193e5bc62d245
JohnJasonFisher <jjfisher91@gmail.com>
2020-03-10 18:52:24 -0500

    sortTradesBySymbol works for 2 unique symbols

--- src/components/AggregatesTable/AggregatesTable.test.js
+++ src/components/AggregatesTable/AggregatesTable.test.js
@@ -2,7 +2,7 @@ import {sortTradesBySymbol} from './AggregatesTable'
 
 describe('sortTradesBySymbol', () => {
 
-	const testTrades = [
+	let testTrades = [
 		{
 			"symbol": "SPX",
 			"price": 2780,
@@ -14,18 +14,33 @@ describe('sortTradesBySymbol', () => {
 			"price": 2770,
 			"shares": 2,
 			"action": "SELL"
+		},
+		{
+			"symbol": "SPY",
+			"price": 277,
+			"shares": 4,
+			"action": "BUY"
 		}
 	]
 
 	test('sortTradesBySymbol returns 1 symbol', () => {
-		const list_with_one_trade = [testTrades[0]]
+		let list_with_one_trade = testTrades.slice(0, 1)
 		const expected_aggregate = {SPX: {symbol: 'SPX', pnl: 2780, total_trades: 1}}
 		expect(sortTradesBySymbol(list_with_one_trade)).toEqual(expected_aggregate)
 	})
 
 	test('sortTradesBySymbol returns 1 unique symbol', () => {
-		const list_with_one_unique_trade = testTrades
+		const list_with_one_unique_trade = testTrades.slice(0, -1)
 		const expected_aggregate = {SPX: {symbol: 'SPX', pnl: 5550, total_trades: 2}}
+		expect(sortTradesBySymbol(list_with_one_unique_trade)).toEqual(expected_aggregate)
+	})
+
+	test('sortTradesBySymbol returns 2 unique symbols', () => {
+		const list_with_one_unique_trade = testTrades
+		const expected_aggregate = {
+			SPX: {symbol: 'SPX', pnl: 5550, total_trades: 2},
+			SPY: {symbol: 'SPY', pnl: 277, total_trades: 1}
+		}
 		expect(sortTradesBySymbol(list_with_one_unique_trade)).toEqual(expected_aggregate)
 	})
 
