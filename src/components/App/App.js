db22b91953afd052f8586fae322e2c4703fcb870
John Fisher <jjfisher91@gmail.com>
2020-05-23 09:50:39 -0500

    add env config error message

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -20,10 +20,17 @@ function App() {
 		fetchData()
 	}, [])
 
+	function showErrorNotification() {
+		if (process.env.NODE_ENV === 'development' && !process.env.REACT_APP_TRADES_URL) {
+			return <h2 style={{color: 'red'}}>Env is has not been configured</h2>
+		}
+	} 
+
 	return (
 	<div className="App">
 		<header className="App-header" style={{'textAlign': 'center'}}>
 			<h1>Trades Project</h1>
+			{showErrorNotification()}
 		</header>
 		<TradesTable trades={tradeData}/>
 		<AggregatesTable trades={tradeData}/>
